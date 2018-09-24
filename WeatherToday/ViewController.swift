f48eb929a60c5edc38dfbc4d423b8bb523623eac
sramavarmax098507 <sajith_ramavarma@contractor.amat.com>
2018-09-24 21:37:45 +0530

    Initial Commit

---
+++ WeatherToday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherToday
+//
+//  Created by Sajith Ramavarma on 24/09/18.
+//  Copyright © 2018 Applied Materials. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WeatherTodayTests/WeatherTodayTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherTodayTests.swift
+//  WeatherTodayTests
+//
+//  Created by Sajith Ramavarma on 24/09/18.
+//  Copyright © 2018 Applied Materials. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherToday
+
+class WeatherTodayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
